1a0dcf36f375cfc215d0d65813db05ab6c1d3935
rynwhite <mailboxofrynwhite@gmail.com>
2026-02-21 19:28:26 +0000

    refactor: replace runWorker with initSystem in syncCommand

--- packages/reference-core/src/cli/sync/index.ts
+++ packages/reference-core/src/cli/sync/index.ts
@@ -1,7 +1,7 @@
 import { log, initLog } from '../lib/log'
 import { initEventBus } from '../event-bus'
 import { loadUserConfig } from '../config'
-import { runWorker } from '../thread-pool'
+import { initSystem } from '../system'
 import { initVirtual } from '../virtual'
 
 // reads config from ui.config.ts, copies user files to codegen, runs Panda codegen and css, generates primitives, and copies final artifacts to node_modules
@@ -9,18 +9,13 @@ import { initVirtual } from '../virtual'
 export const syncCommand = async (cwd: string, options: { watch?: boolean }) => {
   const config = await loadUserConfig(cwd)
 
-  // Initialize event bus first (enables debug logging if configured)
   initEventBus(config)
-
-  // Initialize logging system (sets up log event listeners)
   initLog(config)
-
-  log.debug('Loaded config:', config)
 
   initVirtual(cwd, config, {
     watch: options.watch,
     virtualDir: config.virtualDir,
   })
 
-  await runWorker('system', { config })
+  initSystem(config)
 }
