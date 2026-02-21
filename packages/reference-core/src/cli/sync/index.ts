99aa72531275f80086ae97dd818adac558f29c9b
rynwhite <mailboxofrynwhite@gmail.com>
2026-02-21 10:59:48 +0000

    initLog

--- packages/reference-core/src/cli/sync/index.ts
+++ packages/reference-core/src/cli/sync/index.ts
@@ -1,14 +1,11 @@
-import { log } from '../utils/log'
+import { log, initLog } from '../utils/log'
 import { loadUserConfig } from '../config'
 
 // reads config from ui.config.ts, copies user files to codegen, runs Panda codegen and css, generates primitives, and copies final artifacts to node_modules
 
 export const syncCommand = async (cwd: string, options: { watch?: boolean }) => {
   const config = await loadUserConfig(cwd)
-  
-  if (config.debug) {
-    log.setDebug(true)
-  }
+  initLog(config)
   
   log.debug('Loaded config:', config)
   log('hi from sync command! (stub implementation)')
