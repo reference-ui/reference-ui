4abc0f3edb8cf5ab256840cb5a5a97bd7f69600f
rynwhite <mailboxofrynwhite@gmail.com>
2026-02-25 08:34:45 +0000

    feat: update background color to blue.100 for improved UI consistency

--- packages/reference-docs/src/components/DocLayout/DocSidebar.tsx
+++ packages/reference-docs/src/components/DocLayout/DocSidebar.tsx
@@ -10,7 +10,7 @@ export function DocSidebar() {
       padding="4r"
       borderRightColor="gray.200"
       borderRight="1px solid"
-      background="blue.200"
+      background="blue.100"
       css={{
         position: 'fixed',
         top: 0,
