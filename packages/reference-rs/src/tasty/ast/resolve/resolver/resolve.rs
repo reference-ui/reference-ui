4ee2c3ba8bab42712e977d39dcd531f8a3fdc4af
rynwhite <mailboxofrynwhite@gmail.com>
2026-03-21 21:49:53 +0000

    refactor: enhance type reference handling and modularize resolution logic
    - Added a new `type_ref_map` module to centralize type reference mapping logic, improving code organization and reducing duplication.
    - Refactored `instantiate.rs` and `resolve.rs` to utilize the new mapping functions, streamlining the instantiation and resolution of type references.
    - Updated `REFACTOR.md` to reflect the completion of modularization tasks and the current status of ongoing refactoring efforts.
    - Enhanced documentation in `README.md` to clarify the relationship between `emitted/` types and the generator's string-based code generation approach.

--- packages/reference-rs/src/tasty/ast/resolve/resolver/resolve.rs
+++ packages/reference-rs/src/tasty/ast/resolve/resolver/resolve.rs
@@ -1,162 +1,16 @@
 use super::super::names::namespace_export_lookup_name;
 use super::Resolver;
 use crate::tasty::ast::model::ImportBindingKind;
-use crate::tasty::model::{TupleElement, TypeRef};
+use crate::tasty::model::{
+    TemplateLiteralPart, TsTypeParameter, TupleElement, TypeOperatorKind, TypeRef,
+};
+use crate::tasty::shared::type_ref_map::{map_type_ref, TypeRefMap};
 use crate::tasty::shared::typeref_util::reference_lookup_name;
 
 impl<'a> Resolver<'a> {
     pub(super) fn resolve_type_ref(&self, type_ref: TypeRef) -> TypeRef {
-        match type_ref {
-            TypeRef::Reference {
-                name,
-                target_id,
-                source_module,
-                type_arguments,
-            } => self.resolve_reference(name, target_id, source_module, type_arguments),
-            TypeRef::Union { types } => TypeRef::Union {
-                types: self.resolve_type_refs(types),
-            },
-            TypeRef::Array { element } => TypeRef::Array {
-                element: Box::new(self.resolve_type_ref(*element)),
-            },
-            TypeRef::Tuple { elements } => TypeRef::Tuple {
-                elements: elements
-                    .into_iter()
-                    .map(|element| TupleElement {
-                        element: self.resolve_type_ref(element.element),
-                        ..element
-                    })
-                    .collect(),
-            },
-            TypeRef::Intersection { types } => TypeRef::Intersection {
-                types: self.resolve_type_refs(types),
-            },
-            TypeRef::Object { members } => TypeRef::Object {
-                members: members
-                    .into_iter()
-                    .map(|member| self.resolve_member(member))
-                    .collect(),
-            },
-            TypeRef::IndexedAccess { object, index, .. } => {
-                let resolved_object = self.resolve_type_ref(*object);
-                let resolved_index = self.resolve_type_ref(*index);
-                let resolved = self
-                    .resolve_indexed_access_result(&resolved_object, &resolved_index)
-                    .map(Box::new);
-
-                TypeRef::IndexedAccess {
-                    object: Box::new(resolved_object),
-                    index: Box::new(resolved_index),
-                    resolved,
-                }
-            }
-            TypeRef::Function {
-                params,
-                return_type,
-            } => TypeRef::Function {
-                params: params
-                    .into_iter()
-                    .map(|param| self.resolve_fn_param(param))
-                    .collect(),
-                return_type: Box::new(self.resolve_type_ref(*return_type)),
-            },
-            TypeRef::Constructor {
-                r#abstract,
-                type_parameters,
-                params,
-                return_type,
-            } => TypeRef::Constructor {
-                r#abstract,
-                type_parameters: type_parameters
-                    .into_iter()
-                    .map(|param| self.resolve_type_parameter(param))
-                    .collect(),
-                params: params
-                    .into_iter()
-                    .map(|param| self.resolve_fn_param(param))
-                    .collect(),
-                return_type: Box::new(self.resolve_type_ref(*return_type)),
-            },
-            TypeRef::TypeOperator {
-                operator, target, ..
-            } => {
-                let resolved_target = self.resolve_type_ref(*target);
-                let resolved = self
-                    .resolve_type_operator_result(operator, &resolved_target)
-                    .map(Box::new);
-
-                TypeRef::TypeOperator {
-                    operator,
-                    target: Box::new(resolved_target),
-                    resolved,
-                }
-            }
-            TypeRef::TypeQuery { expression, .. } => TypeRef::TypeQuery {
-                resolved: self
-                    .resolve_type_query_expression(&expression)
-                    .map(|resolved| Box::new(self.resolve_type_ref(resolved))),
-                expression,
-            },
-            TypeRef::Conditional {
-                check_type,
-                extends_type,
-                true_type,
-                false_type,
-                ..
-            } => {
-                let resolved_check_type = self.resolve_type_ref(*check_type);
-                let resolved_extends_type = self.resolve_type_ref(*extends_type);
-                let resolved_true_type = self.resolve_type_ref(*true_type);
-                let resolved_false_type = self.resolve_type_ref(*false_type);
-                let resolved = self
-                    .resolve_conditional_result(
-                        &resolved_check_type,
-                        &resolved_extends_type,
-                        &resolved_true_type,
-                        &resolved_false_type,
-                    )
-                    .map(Box::new);
-
-                TypeRef::Conditional {
-                    check_type: Box::new(resolved_check_type),
-                    extends_type: Box::new(resolved_extends_type),
-                    true_type: Box::new(resolved_true_type),
-                    false_type: Box::new(resolved_false_type),
-                    resolved,
-                }
-            }
-            TypeRef::Mapped {
-                type_param,
-                source_type,
-                name_type,
-                optional_modifier,
-                readonly_modifier,
-                value_type,
-            } => TypeRef::Mapped {
-                type_param,
-                source_type: Box::new(self.resolve_type_ref(*source_type)),
-                name_type: name_type.map(|name_type| Box::new(self.resolve_type_ref(*name_type))),
-                optional_modifier,
-                readonly_modifier,
-                value_type: value_type
-                    .map(|value_type| Box::new(self.resolve_type_ref(*value_type))),
-            },
-            TypeRef::TemplateLiteral { parts, .. } => {
-                let resolved_parts = parts
-                    .into_iter()
-                    .map(|part| self.resolve_template_literal_part(part))
-                    .collect::<Vec<_>>();
-                let resolved = self
-                    .resolve_template_literal_result(&resolved_parts)
-                    .map(Box::new);
-
-                TypeRef::TemplateLiteral {
-                    parts: resolved_parts,
-                    resolved,
-                }
-            }
-            other => other,
-        }
+        let mut mapper = ResolverTypeRefMap { resolver: self };
+        map_type_ref(&mut mapper, type_ref)
     }
 
     fn resolve_reference(
@@ -227,5 +81,119 @@ impl<'a> Resolver<'a> {
         }
 
         Some(current)
+    }
+}
+
+struct ResolverTypeRefMap<'a, 'b> {
+    resolver: &'a Resolver<'b>,
+}
+
+impl TypeRefMap for ResolverTypeRefMap<'_, '_> {
+    fn map_reference(
+        &mut self,
+        name: String,
+        target_id: Option<String>,
+        source_module: Option<String>,
+        type_arguments: Option<Vec<TypeRef>>,
+    ) -> TypeRef {
+        self.resolver
+            .resolve_reference(name, target_id, source_module, type_arguments)
+    }
+
+    fn map_indexed_access(
+        &mut self,
+        object: TypeRef,
+        index: TypeRef,
+        _resolved: Option<Box<TypeRef>>,
+    ) -> TypeRef {
+        let resolved = self
+            .resolver
+            .resolve_indexed_access_result(&object, &index)
+            .map(Box::new);
+        TypeRef::IndexedAccess {
+            object: Box::new(object),
+            index: Box::new(index),
+            resolved,
+        }
+    }
+
+    fn map_type_operator(
+        &mut self,
+        operator: TypeOperatorKind,
+        target: TypeRef,
+        _resolved: Option<Box<TypeRef>>,
+    ) -> TypeRef {
+        let resolved = self
+            .resolver
+            .resolve_type_operator_result(operator, &target)
+            .map(Box::new);
+        TypeRef::TypeOperator {
+            operator,
+            target: Box::new(target),
+            resolved,
+        }
+    }
+
+    fn map_type_query(
+        &mut self,
+        expression: String,
+        _resolved: Option<Box<TypeRef>>,
+    ) -> TypeRef {
+        TypeRef::TypeQuery {
+            resolved: self
+                .resolver
+                .resolve_type_query_expression(&expression)
+                .map(|resolved| Box::new(self.resolver.resolve_type_ref(resolved))),
+            expression,
+        }
+    }
+
+    fn map_conditional(
+        &mut self,
+        check_type: TypeRef,
+        extends_type: TypeRef,
+        true_type: TypeRef,
+        false_type: TypeRef,
+        _resolved: Option<Box<TypeRef>>,
+    ) -> TypeRef {
+        let resolved = self
+            .resolver
+            .resolve_conditional_result(
+                &check_type,
+                &extends_type,
+                &true_type,
+                &false_type,
+            )
+            .map(Box::new);
+        TypeRef::Conditional {
+            check_type: Box::new(check_type),
+            extends_type: Box::new(extends_type),
+            true_type: Box::new(true_type),
+            false_type: Box::new(false_type),
+            resolved,
+        }
+    }
+
+    fn map_template_literal(
+        &mut self,
+        parts: Vec<TemplateLiteralPart>,
+        _resolved: Option<Box<TypeRef>>,
+    ) -> TypeRef {
+        let resolved = self
+            .resolver
+            .resolve_template_literal_result(&parts)
+            .map(Box::new);
+        TypeRef::TemplateLiteral { parts, resolved }
+    }
+
+    fn map_type_parameter(&mut self, param: TsTypeParameter) -> TsTypeParameter {
+        self.resolver.resolve_type_parameter(param)
+    }
+
+    fn map_tuple_element(&mut self, element: TupleElement) -> TupleElement {
+        TupleElement {
+            element: map_type_ref(self, element.element),
+            ..element
+        }
     }
 }
